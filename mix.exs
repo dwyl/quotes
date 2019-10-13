9b7c3631248b9a3dcb4a7543df24325acabd41e7
nelsonic <nelson+github@dwyl.io>
2019-10-13 08:23:20 +0100

    remove comments from deps list

--- mix.exs
+++ mix.exs
@@ -22,8 +22,6 @@ defmodule Quotes.MixProject do
   defp deps do
     [
       {:jason, "~> 1.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
