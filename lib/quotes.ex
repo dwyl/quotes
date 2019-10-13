38bd7c1b5ed12248a13ffa7f786919e3da5772ac
nelsonic <nelson+github@dwyl.io>
2019-10-13 22:40:05 +0100

    make test for Quotes.parse_json() pass #4 ✅

--- lib/quotes.ex
+++ lib/quotes.ex
@@ -15,4 +15,30 @@ defmodule Quotes do
   def hello do
     :world
   end
+
+  @doc """
+  parse_json returns a list of maps with quotes in the following form:
+  [
+    %{
+      "author" => "Albert Einstein",
+      "text" => "Once we accept our limits, we go beyond them."
+    },
+    %{
+      "author" => "Peter Drucker",
+      "source" => "https://www.goodreads.com/quotes/784267",
+      "tags" => "time, management",
+      "text" => "Until we can manage time, we can manage nothing else."
+    }
+    %{...},
+    ...
+  ]
+
+  All quotes MUST have an `author` and `text` field.
+  Some quotes have `tags` and `source`, please help to expand/verify others.
+  see: https://github.com/nelsonic/quotes#contributing
+  """
+  def parse_json do
+    File.read!("quotes.json") |> Jason.decode!()
+  end
+
 end
