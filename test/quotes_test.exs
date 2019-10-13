0d02d8738a519a4e4eb27832fe8e063119a987ed
nelsonic <nelson+github@dwyl.io>
2019-10-13 22:39:57 +0100

    add test for Quotes.parse_json() #4 🔴

--- test/quotes_test.exs
+++ test/quotes_test.exs
@@ -5,4 +5,25 @@ defmodule QuotesTest do
   test "greets the world" do
     assert Quotes.hello() == :world
   end
+
+  test "parse_json returns a list of maps containing quotes" do
+    list = Quotes.parse_json()
+    assert Enum.count(list) == Utils.count()
+
+    # sample quote we know is in the list
+    sample =  %{
+      "author" => "Albert Einstein",
+      "text" => "A person who never made a mistake never tried anything new."
+    }
+
+    # find the sample quote in the List of Maps:
+    [found] = Enum.map(list, fn q ->
+      if q["author"] == sample["author"] && q["text"] == sample["text"] do
+        q
+      end
+    end)
+    |> Enum.filter(& !is_nil(&1)) # filter out any nil values
+    assert sample == found # sample quote was found in the list
+  end
+
 end
