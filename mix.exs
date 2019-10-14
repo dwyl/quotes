db25d687ce318130dbf2475c1d47aeed4c42adcd
nelsonic <nelson+github@dwyl.io>
2019-10-14 17:26:26 +0100

    add package metadata to mix.exs #4

--- mix.exs
+++ mix.exs
@@ -5,9 +5,16 @@ defmodule Quotes.MixProject do
     [
       app: :quotes,
       version: "0.1.0",
+      package: package(),
+      description: description(),
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.json": :test,
+      ]
     ]
   end
 
@@ -26,4 +33,17 @@ defmodule Quotes.MixProject do
       {:ex_doc, "~> 0.21", only: :dev}
     ]
   end
+
+  defp description() do
+    "quotes is a collection of inspiring quotes and methods to return them."
+  end
+
+  defp package() do
+    [
+      name: "quotes",
+      licenses: ["GNU GPL v2.0"],
+      maintainers: ["dwyl"],
+      links: %{"GitHub" => "https://github.com/nelsonic/quotes"}
+    ]
+end
 end
