a6fd447e2d2526239a659bc3fca7edcc74983601
nelsonic <nelson+github@dwyl.io>
2019-10-15 10:16:20 +0100

    add test/utils_test.exs for completeness 💯

---
+++ test/utils_test.exs
@@ -0,0 +1,21 @@
+defmodule UtilsTest do
+  use ExUnit.Case
+
+  test "clean up data in quotes.json" do
+    Utils.clean()
+    # Confirm there are no anonymous authors:
+    list = File.read!("quotes.json")
+    |> Jason.decode!
+    |> Enum.uniq
+    |> Enum.map(fn q -> # quote must not be from unknown authors
+      if q["author"] == ""
+      || q["author"] == nil
+      || q["author"] == "unknown"
+      || q["author"] == "anonymous"do
+        q
+      end
+    end)
+    |> Enum.filter(& !is_nil(&1))
+    assert Enum.count(list) == 0
+  end
+end
