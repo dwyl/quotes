de6e0eef57fef25bef47295c730c7b23923e95b8
Luís Arteiro <lucho14garfield@gmail.com>
2023-05-17 16:25:24 +0100

    feat: Adding features and test. #96

--- lib/quotesy.dart
+++ lib/quotesy.dart
@@ -5,6 +5,7 @@ library quotesy;
 
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 class QuoteInfo {
   final String author;
@@ -21,10 +22,34 @@ class QuoteInfo {
 }
 
 class Quotes {
-  static Future<List<QuoteInfo>> loadQuotes() async {
+  static Future<List<QuoteInfo>> _quotesList() async {
     String jsonString = await File('quotes.json').readAsString();
     List<dynamic> jsonList = json.decode(jsonString);
     List<QuoteInfo> quotes = jsonList.map((json) => QuoteInfo.fromJson(json)).toList();
     return quotes;
+  }
+
+  static list() async {
+    return await _quotesList();
+  }
+
+  static Future<QuoteInfo> random() async {
+    final random = Random();
+    final list = await _quotesList();
+
+    return list[random.nextInt(list.length)];
+  }
+
+  static Future<List<QuoteInfo>> byAuthor(String author) async {
+    final list = await _quotesList();
+    return list.where((quote) => quote.author == author).toList();
+  }
+
+  static Future<QuoteInfo> singleRandomByAuthor(String author) async {
+    final list = await _quotesList();
+    final listByAuthor = list.where((quote) => quote.author == author).toList();
+    final random = Random();
+
+    return listByAuthor[random.nextInt(listByAuthor.length)];
   }
 }
