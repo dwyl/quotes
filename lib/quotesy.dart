9b6713a6de4071875c205f34fbd14c5f4a47b0c7
Luís Arteiro <lucho14garfield@gmail.com>
2023-05-17 14:22:53 +0100

    feat: Adding Dart package template files. #96

---
+++ lib/quotesy.dart
@@ -0,0 +1,10 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library quotesy;
+
+class Awesome {
+  bool get isAwesome => true;
+}
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ test/quotesy_test.dart
@@ -0,0 +1,16 @@
+import 'package:quotesy/quotesy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
