325bae716a31d16412470404911dba435d4fa37a
Luís Arteiro <lucho14garfield@gmail.com>
2023-05-18 11:45:04 +0100

    feat: Adding comments. #96

--- lib/quotesy.dart
+++ lib/quotesy.dart
@@ -1,27 +1,32 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library quotesy;
 
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
+/// Class holding a quote information
 class QuoteInfo {
   final String author;
   final String text;
+  final String? source;
+  final String? tags;
 
-  QuoteInfo(this.text, this.author);
+  QuoteInfo(this.text, this.author, {this.source, this.tags});
 
   factory QuoteInfo.fromJson(Map<String, dynamic> json) {
     return QuoteInfo(
       json['text'],
       json['author'],
+      source: json['source'],
+      tags: json['tags'],
     );
   }
 }
 
+/// Static class that can be invoked to fetch quotes
 class Quotes {
+
+  /// Loads the information from the `.json` file containing the quotes list.
   static Future<List<QuoteInfo>> _quotesList() async {
     String jsonString = await File('quotes.json').readAsString();
     List<dynamic> jsonList = json.decode(jsonString);
@@ -29,10 +34,12 @@ class Quotes {
     return quotes;
   }
 
+  /// Returns a list of quotes
   static list() async {
     return await _quotesList();
   }
 
+  /// Returns a random quote
   static Future<QuoteInfo> random() async {
     final random = Random();
     final list = await _quotesList();
@@ -40,11 +47,13 @@ class Quotes {
     return list[random.nextInt(list.length)];
   }
 
+  /// Returns a list of quotes by author
   static Future<List<QuoteInfo>> byAuthor(String author) async {
     final list = await _quotesList();
     return list.where((quote) => quote.author == author).toList();
   }
 
+  /// Returns a random quote by author
   static Future<QuoteInfo> singleRandomByAuthor(String author) async {
     final list = await _quotesList();
     final listByAuthor = list.where((quote) => quote.author == author).toList();
